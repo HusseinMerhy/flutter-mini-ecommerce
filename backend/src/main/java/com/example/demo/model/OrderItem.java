dbd4bcd819b37e1ebc7922b898db7fa56ff402bb
HusseinM20 <130611739+HusseinM20@users.noreply.github.com>
2025-09-08 23:19:11 +0300

    refactor: update OrderItem model with JSON ignore annotations

--- backend/src/main/java/com/example/demo/model/OrderItem.java
+++ backend/src/main/java/com/example/demo/model/OrderItem.java
@@ -1,2 +1,81 @@
-package com.example.demo.model;public class OrderItem {
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
+
+	@Column(nullable = false)
+	private Integer quantity;
+
+	@Column(nullable = false)
+	private BigDecimal price;
+
+	@ManyToOne
+	@JoinColumn(name = "order_id", nullable = false)
+	@JsonIgnore
+	private Order order;
+
+
+	public OrderItem() {
+	}
+
+
+	public OrderItem (Product product, Integer quantity, BigDecimal price) {
+		this.product = product;
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = BigDecimal.valueOf(price);
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
 }
