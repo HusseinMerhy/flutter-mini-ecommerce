6796c737dc6ce21aae0d299ae75b78b6fffed734
HusseinM20 <130611739+HusseinM20@users.noreply.github.com>
2025-09-08 23:19:53 +0300

    feat: update ProductController with admin authorization

--- backend/src/main/java/com/example/demo/controller/ProductController.java
+++ backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,2 +1,58 @@
-package com.example.demo.controller;public class ProductController {
+package com.example.demo.controller;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+		List<Product>products =productService.getAllProducts();
+		return ResponseEntity.ok(products);
+	}
+	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+		Product savedProduct = productService.addProduct(product);
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
+	}
+	@GetMapping("/low-stock")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public ResponseEntity<List<Product>> getLowStockProducts(@RequestParam int threshold) {
+		List<Product> lowStockProducts = productService.getLowStockProducts(threshold);
+		return ResponseEntity.ok(lowStockProducts);
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+		Optional<Product> product = productService.getProductById(id);
+		return product.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	@PutMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public ResponseEntity<Product> editProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
+
+		Product savedProduct = productService.editProduct(id, updatedProduct);
+		return ResponseEntity.ok(savedProduct);
+	}
+	@DeleteMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+		productService.deleteProduct(id);
+		return ResponseEntity.noContent().build(); // 204 No Content
+	}
+
 }
