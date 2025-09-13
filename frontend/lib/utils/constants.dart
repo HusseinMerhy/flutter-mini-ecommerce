35fc21316bec4284a7cc05da6782edfffafb11b1
HusseinM20 <130611739+HusseinM20@users.noreply.github.com>
2025-09-13 05:11:25 +0300

    Update constants for application-wide values and configurations

---
+++ frontend/lib/utils/constants.dart
@@ -0,0 +1,9 @@
+// lib/utils/constants.dart
+class AppConstants {
+  static const String appName = 'E-Commerce App';
+
+
+static const String apiBaseUrl = 'http://localhost:8080/api';
+
+// Add other constants as needed
+}// TODO Implement this library.
