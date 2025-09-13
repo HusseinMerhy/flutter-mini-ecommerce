66e5bcf3b5c5c1d66f8df39bce7a0e1a7dfd9ad3
HusseinM20 <130611739+HusseinM20@users.noreply.github.com>
2025-09-13 05:10:54 +0300

    Update Product model for product data structure

--- frontend/lib/models/Product.dart
+++ frontend/lib/models/Product.dart
@@ -1,4 +1,45 @@
-package models;
 
-public class Product {
+class Product {
+  final String id;
+  final String name;
+  final double price;
+  final int stock;
+  final String? imageUrl;
+  final String? description;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.stock,
+    this.imageUrl,
+    this.description,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'].toString(),
+      name: json['name'],
+      price: json['price'] is int
+          ? (json['price'] as int).toDouble()
+          : json['price'],
+      stock: json['stock'],
+      imageUrl: json['imageUrl'],
+      description: json['description'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'stock': stock,
+      'imageUrl': imageUrl,
+      'description': description,
+    };
+  }
+
+  bool get isOutOfStock => stock == 0;
+  bool get isLowStock => stock > 0 && stock < 5;
 }
